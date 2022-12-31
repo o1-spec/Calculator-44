a2d50f2ceab02828f1c6203e8582d9e9e53024be
o1-spec <oluwafemionadokun@gmail.com>
2022-12-31 21:42:52 +0100

    Initial Commit

---
+++ calculator 4.js
@@ -0,0 +1,37 @@
+let display = document.getElementById('display')
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1)
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error';
+                }
+                //display.innerText = eval(display.innerText);
+                break;
+            default:
+                display.innerHTML += e.target.innerHTML;
+
+        }
+
+
+
+        /*
+        console.log('clicked')
+        console.log(e)
+        console.log(e.length)
+        console.log(e.target.innerHtml)*/
+    })
+})
